51fdb3f82bc7213bab725724a696f95ac9314c76
Cássio Botaro <cassiobotaro@gmail.com>
2017-06-28 13:32:42 -0300

    fix one more typo
    s/alotted/allotted/g

--- errors.go
+++ errors.go
@@ -9,7 +9,7 @@ var (
 	errUnknown   = fmt.Errorf("%s: Error unknown", libraryName)
 	errLibrary   = fmt.Errorf("%s: Library-specific error", libraryName)
 	errStatus400 = fmt.Errorf("%s: Your request was malformed", libraryName)
-	errStatus403 = fmt.Errorf("%s: Your API key is invalid, missing, or has exceeded its quota. **Paid plans will not receive a 403 response when they exceed their alotted matches. They will only receive a 403 for exceeding rate limit quotas", libraryName)
+	errStatus403 = fmt.Errorf("%s: Your API key is invalid, missing, or has exceeded its quota. **Paid plans will not receive a 403 response when they exceed their allotted matches. They will only receive a 403 for exceeding rate limit quotas", libraryName)
 	errStatus404 = fmt.Errorf("%s: This person was searched in the past 24 hours and nothing was found", libraryName)
 	errStatus405 = fmt.Errorf("%s: You have queried the API with an unsupported HTTP method. Retry your query with either GET or POST", libraryName)
 	errStatus422 = fmt.Errorf("%s: Invalid or missing API query parameter", libraryName)
